c90d667f26f2e6f0b8666aa1c4403b12c36cd8df
Hans-Ulrich Lutter <hu.lutter@gmail.com>
2017-05-01 15:28:12 +0200

    Added InjectionToken for ApiService configuration

--- src/app/data/data.module.ts
+++ src/app/data/data.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ApiService } from './services/api.service';
-import { UserService } from './services/user.service';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ApiService} from './services/api.service';
+import {UserService} from './services/user.service';
+import {HttpModule} from '@angular/http';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpModule
   ],
   providers: [ApiService, UserService]
 })
-export class DataModule { }
+export class DataModule {
+}
--- src/app/data/services/api.service.ts
+++ src/app/data/services/api.service.ts
@@ -1,28 +1,33 @@
 import {Http} from '@angular/http';
-import {Injectable} from '@angular/core';
+import {Inject, Injectable, InjectionToken} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+
+export interface ApiServiceConfig {
+  baseUrl: string;
+}
+
+export const API_SERVICE_CONFIG = new InjectionToken<ApiServiceConfig>('ApiConfig');
 
 @Injectable()
 export class ApiService {
 
-  private baseUrl = 'http://jsonplaceholder.typicode.com';
-
-  constructor(private http: Http) {
-    console.log(`The following url has been registered as ApiRoot: ${this.baseUrl}`);
+  constructor(private http: Http, @Inject(API_SERVICE_CONFIG) private config: ApiServiceConfig) {
+    console.log(`The following url has been registered as ApiRoot: ${this.config.baseUrl}`);
   }
 
   requestResource(resourcePath: string): Observable<any> {
-    const resourceUrl = this.baseUrl + resourcePath;
+    const resourceUrl = this.config.baseUrl + resourcePath;
     return this.http
                .get(resourceUrl)
                .map(response => response.json())
                .catch(error => this.handleError(error, resourcePath));
   }
 
-  private handleError (error: Response | any, resourcePath: string): Observable<any> {
+  private handleError(error: Response | any, resourcePath: string): Observable<any> {
     const result = (error instanceof Response) ? error.json() : error;
     const message = `Requesting the resource '${resourcePath}' an error occurred.`;
     console.error(message, result);
